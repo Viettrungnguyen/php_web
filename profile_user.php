5f61a2a9381add91746a787ba37a39a73884837c
Trung Nguyen <trung.nguyen@kiaisoft.com>
2023-11-02 18:02:22 +0700

    done change info user

--- profile_user.php
+++ profile_user.php
@@ -1,49 +1,46 @@
-<?php
-session_start();
-
-if (!isset($_SESSION['user_id'])) {
-    header("Location: login.php");
-    exit();
-}
-
-$host = "localhost";
-$username = "root";
-$password = "";
-$database = "web_sell_clother";
-
-$conn = new mysqli($host, $username, $password, $database);
-
-if ($conn->connect_error) {
-    die("Database connection failed: " . $conn->connect_error);
-}
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_POST['update_profile'])) {
-        $newUsername = $_POST['new_username'];
-        $userId = $_SESSION['user_id'];
-
-        $sql = "UPDATE users SET username = ? WHERE id = ?";
-        $stmt = $conn->prepare($sql);
-        $stmt->bind_param("si", $newUsername, $userId);
-
-        if ($stmt->execute()) {
-            $_SESSION['username'] = $newUsername;
-            $success_message = "Your username has been updated!";
-        } else {
-            $error_message = "Failed to update username.";
-        }
-
-        $stmt->close();
-    }
-}
-?>
-
 <!DOCTYPE html>
 <html>
 
 <head>
     <title>Change Information</title>
     <link rel="stylesheet" type="text/css" href="stylePage.css">
+    <style>
+        /* Add your custom CSS styles here */
+        .container {
+            text-align: center;
+        }
+
+        form {
+            max-width: 400px;
+            margin: 0 auto;
+        }
+
+        label {
+            display: block;
+            margin-top: 10px;
+        }
+
+        input[type="text"] {
+            width: 100%;
+            padding: 8px;
+            margin: 6px 0;
+            box-sizing: border-box;
+        }
+
+        input[type="submit"] {
+            background-color: #4CAF50;
+            color: white;
+            border: none;
+            padding: 10px 20px;
+            margin-top: 10px;
+            cursor: pointer;
+            border-radius: 5px;
+        }
+
+        input[type="submit"]:hover {
+            background-color: #45a049;
+        }
+    </style>
 </head>
 
 <body>
@@ -62,6 +59,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <form method="post">
             <label for="new_username">New Username:</label>
             <input type="text" id="new_username" name="new_username" required>
+            <label for="new_phone">New Phone:</label>
+            <input type="text" id="new_phone" name="new_phone" required>
+            <label for="new_address">New Address:</label>
+            <input type="text" id="new_address" name="new_address" required>
             <input type="submit" name="update_profile" value="Update Profile">
         </form>
     </main>
